6202a0394a824c54aa9f9f4418ea8326e53a706d
Mauxell <mcs.aplicacoes@gmail.com>
2016-12-07 22:05:56 -0200

    Dano inicio ao uso do JQuery

--- principal.js
+++ principal.js
@@ -10,3 +10,44 @@ document.querySelector("#mudaLayout").addEventListener("click", function(){
     this.textContent = "Linhas";
   }
 });
+
+function removeCartao() {
+  var cartao = document.querySelector("#cartao_" + this.getAttribute("data-ref"));
+
+  cartao.classList.add("cartao--some");
+
+  setTimeout(function(){
+    cartao.remove();
+  },400);
+}
+
+var botoes = document.querySelectorAll(".opcoesDoCartao-remove");
+
+for (var i = 0; i < botoes.length; i++){
+  botoes[i].addEventListener("click", removeCartao);
+};
+
+
+let $novoCartao = document.querySelector(".novoCartao");
+let $novoCartaoConteudo = document.querySelector(".novoCartao-conteudo");
+
+$novoCartaoConteudo.addEventListener("input", function(){
+  let $error = document.querySelector(".error");
+  if ($error){
+    $error.remove();
+  }
+
+})
+
+$novoCartao.addEventListener("submit", function (event){
+  event.preventDefault();
+  if (this.querySelector(".novoCartao-conteudo").value =='' &&
+  !this.querySelector(".error")){
+    let $msgError = document.createElement("span");
+    $msgError.classList.add("error");
+    $msgError.textContent = "Preencha o campo acima";
+
+    this.insertBefore($msgError, document.querySelector(".novoCartao-salvar"))
+  };
+
+});
